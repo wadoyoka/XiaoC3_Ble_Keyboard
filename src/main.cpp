1328e473d2ff921792b50786cec134cad667d585
wadoyoka <atsushi021028@gmail.com>
2025-04-05 01:45:27 +0900

    Update: flagmentation & Add LedPin

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,28 @@
 
 BleKeyboard bleKeyboard("XiaoC3_Media");
 
-#define PIN_WRITE 20 // GPIO2を使用する
 #define PIN_READ 2
+#define LED_PIN 20
 
 unsigned long previousMillis = 0; // will store last time LED was updated
 const long interval = 1 * 200;    // interval at which to blink (milliseconds)
 
-unsigned long lastActivityTime = 0;               // 最後にボタンが押された時刻を記録
-const unsigned long inactivityThreshold = 60000;  // 1分（60000ミリ秒）操作がない場合にスリープするよう設定
+unsigned long lastActivityTime = 0;              // 最後にボタンが押された時刻を記録
+const unsigned long inactivityThreshold = 60000; // 1分（60000ミリ秒）操作がない場合にスリープするよう設定
+
+bool isPressed = false; // 長押し対策
+const unsigned long holdInterval = 2 * 1000;
 
 void setup()
 {
   // PIN出力モード
-  pinMode(PIN_WRITE, OUTPUT);
   pinMode(PIN_READ, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
-  digitalWrite(PIN_WRITE, HIGH);
   bleKeyboard.begin();
   // GPIO2が LOW（ボタンが押された状態）になるとESP32をスリープ解除
   esp_deep_sleep_enable_gpio_wakeup(BIT(PIN_READ), ESP_GPIO_WAKEUP_GPIO_LOW);
+  digitalWrite(LED_PIN, HIGH);
 }
 
 void loop()
@@ -35,15 +38,24 @@ void loop()
     previousMillis = currentMillis;
     if (bleKeyboard.isConnected())
     {
-      Serial.println("Sending Play/Pause media key...");
-      bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
-      Serial.println("Sending Play/Pause media key Complete!");
+      if (!isPressed)
+      {
+        Serial.println("Sending Play/Pause media key...");
+        bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+        Serial.println("Sending Play/Pause media key Complete!");
+        isPressed = true;
+      }
     }
   }
+  else if (digitalRead(PIN_READ) == 1)
+  {
+    isPressed = false;
+  }
 
-  if (millis() - lastActivityTime > inactivityThreshold) {
+  if (millis() - lastActivityTime > inactivityThreshold)
+  {
     Serial.println("Go to DeepSleep!!");
-    delay(1000);             // 少し待ってからスリープ
-    esp_deep_sleep_start();  // ディープスリープに入る
+    delay(1000);            // 少し待ってからスリープ
+    esp_deep_sleep_start(); // ディープスリープに入る
   }
 }
